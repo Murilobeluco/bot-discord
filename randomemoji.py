a010b1f8c2d4aeb3474d473e84dc676b4cf74e27
murilo beluco <murilobeluco@yahoo.com>
2020-02-02 10:06:16 -0300

    comando ping e tratamento de erro

--- randomemoji.py
+++ randomemoji.py
@@ -6,89 +6,89 @@ from unicodedata import name as unicode_name
 UNICODE_VERSION = 6
 
 EMOJI_RANGES_UNICODE = {
-    6: [
-        ('\U0001F300', '\U0001F320'),
-        ('\U0001F330', '\U0001F335'),
-        ('\U0001F337', '\U0001F37C'),
-        ('\U0001F380', '\U0001F393'),
-        ('\U0001F3A0', '\U0001F3C4'),
-        ('\U0001F3C6', '\U0001F3CA'),
-        ('\U0001F3E0', '\U0001F3F0'),
-        ('\U0001F400', '\U0001F43E'),
-        ('\U0001F440', ),
-        ('\U0001F442', '\U0001F4F7'),
-        ('\U0001F4F9', '\U0001F4FC'),
-        ('\U0001F500', '\U0001F53C'),
-        ('\U0001F540', '\U0001F543'),
-        ('\U0001F550', '\U0001F567'),
-        ('\U0001F5FB', '\U0001F5FF')
-    ],
-    7: [
-        ('\U0001F300', '\U0001F32C'),
-        ('\U0001F330', '\U0001F37D'),
-        ('\U0001F380', '\U0001F3CE'),
-        ('\U0001F3D4', '\U0001F3F7'),
-        ('\U0001F400', '\U0001F4FE'),
-        ('\U0001F500', '\U0001F54A'),
-        ('\U0001F550', '\U0001F579'),
-        ('\U0001F57B', '\U0001F5A3'),
-        ('\U0001F5A5', '\U0001F5FF')
-    ],
-    8: [
-        ('\U0001F300', '\U0001F579'),
-        ('\U0001F57B', '\U0001F5A3'),
-        ('\U0001F5A5', '\U0001F5FF')
-    ]
+	6: [
+		('\U0001F300', '\U0001F320'),
+		('\U0001F330', '\U0001F335'),
+		('\U0001F337', '\U0001F37C'),
+		('\U0001F380', '\U0001F393'),
+		('\U0001F3A0', '\U0001F3C4'),
+		('\U0001F3C6', '\U0001F3CA'),
+		('\U0001F3E0', '\U0001F3F0'),
+		('\U0001F400', '\U0001F43E'),
+		('\U0001F440', ),
+		('\U0001F442', '\U0001F4F7'),
+		('\U0001F4F9', '\U0001F4FC'),
+		('\U0001F500', '\U0001F53C'),
+		('\U0001F540', '\U0001F543'),
+		('\U0001F550', '\U0001F567'),
+		('\U0001F5FB', '\U0001F5FF')
+	],
+	7: [
+		('\U0001F300', '\U0001F32C'),
+		('\U0001F330', '\U0001F37D'),
+		('\U0001F380', '\U0001F3CE'),
+		('\U0001F3D4', '\U0001F3F7'),
+		('\U0001F400', '\U0001F4FE'),
+		('\U0001F500', '\U0001F54A'),
+		('\U0001F550', '\U0001F579'),
+		('\U0001F57B', '\U0001F5A3'),
+		('\U0001F5A5', '\U0001F5FF')
+	],
+	8: [
+		('\U0001F300', '\U0001F579'),
+		('\U0001F57B', '\U0001F5A3'),
+		('\U0001F5A5', '\U0001F5FF')
+	]
 }
 
 NO_NAME_ERROR = '(No name found for this codepoint)'
 
 def quantos_dias():
-    from datetime import datetime
-    import random
-    nomes = ['Biroliro','Bostonaro','LiroBiro', 'Bolonoro', 'Boldonaro', 'Bobonaro', 'Bolinaro', 'Bololonaro', 'Laranja', 'Coiso', 'Bozo']
-    now = datetime.now()
+	from datetime import datetime
+	import random
+	nomes = ['Biroliro','Bostonaro','LiroBiro', 'Bolonoro', 'Boldonaro', 'Bobonaro', 'Bolinaro', 'Bololonaro', 'Laranja', 'Coiso', 'Bozo']
+	now = datetime.now()
 
-    d2 = datetime.strptime('2022-12-31', '%Y-%m-%d')
+	d2 = datetime.strptime('2022-12-31', '%Y-%m-%d')
 
-    quantidade_dias = abs((now - d2).days)
-    return "Ainda vamos ter que aguentar o {nome} por mais {dias} dias :point_right: :point_right: :flag_br:".format(dias=quantidade_dias, nome=random.choice(nomes))
+	quantidade_dias = abs((now - d2).days)
+	return "Ainda vamos ter que aguentar o {nome} por mais {dias} dias :point_right: :point_right: :flag_br:".format(dias=quantidade_dias, nome=random.choice(nomes))
 
 def audio_random_pedra():
-    import random
-    audio = ['caralhovamosfazerpedra.mp3', 'cadepedra.mp3', 'vamosfazerpedra.mp3']
-    escolha = random.choice(audio)
-    return escolha
-    
+	import random
+	audio = ['caralhovamosfazerpedra.mp3', 'cadepedra.mp3', 'vamosfazerpedra.mp3']
+	escolha = random.choice(audio)
+	return escolha
+	
 def audio_random_blizz():
-    import random
-    audio = ['infoblizzard.mp3', 'wowbug.mp3', 'wowbugado.mp3']
-    escolha = random.choice(audio)
-    return escolha
-    
+	import random
+	audio = ['infoblizzard.mp3', 'wowbug.mp3', 'wowbugado.mp3']
+	escolha = random.choice(audio)
+	return escolha
+	
 def random_emoji(unicode_version = 6):
-    if unicode_version in EMOJI_RANGES_UNICODE:
-        emoji_ranges = EMOJI_RANGES_UNICODE[unicode_version]
-    else:
-        emoji_ranges = EMOJI_RANGES_UNICODE[-1]
+	if unicode_version in EMOJI_RANGES_UNICODE:
+		emoji_ranges = EMOJI_RANGES_UNICODE[unicode_version]
+	else:
+		emoji_ranges = EMOJI_RANGES_UNICODE[-1]
 
-    count = [ord(r[-1]) - ord(r[0]) + 1 for r in emoji_ranges]
-    weight_distr = list(accumulate(count))
+	count = [ord(r[-1]) - ord(r[0]) + 1 for r in emoji_ranges]
+	weight_distr = list(accumulate(count))
 
-    point = randrange(weight_distr[-1])
+	point = randrange(weight_distr[-1])
 
-    emoji_range_idx = bisect(weight_distr, point)
-    emoji_range = emoji_ranges[emoji_range_idx]
+	emoji_range_idx = bisect(weight_distr, point)
+	emoji_range = emoji_ranges[emoji_range_idx]
 
-    point_in_range = point
-    if emoji_range_idx is not 0:
-        point_in_range = point - weight_distr[emoji_range_idx - 1]
+	point_in_range = point
+	if emoji_range_idx is not 0:
+		point_in_range = point - weight_distr[emoji_range_idx - 1]
 
-    emoji = chr(ord(emoji_range[0]) + point_in_range)
-    emoji_name = unicode_name(emoji, NO_NAME_ERROR).capitalize()
-    emoji_codepoint = "U+{}".format(hex(ord(emoji))[2:].upper())
+	emoji = chr(ord(emoji_range[0]) + point_in_range)
+	emoji_name = unicode_name(emoji, NO_NAME_ERROR).capitalize()
+	emoji_codepoint = "U+{}".format(hex(ord(emoji))[2:].upper())
 
-    return (emoji, emoji_codepoint, emoji_name)
+	return (emoji, emoji_codepoint, emoji_name)
 
 def desenho(emoji):
   return "⠀ ⠀ ⠀  :cowboy:\n　      {emoji}{emoji}\n    {emoji}   {emoji}　{emoji}\n   :wave:  {emoji}{emoji} :thumbsup:\n  　  {emoji}　{emoji}\n　   {emoji}　 {emoji}\n　     :boot:    :boot:\nSheriff feito de: '{emoji_nome}'".format(emoji=emoji[0], emoji_nome=emoji[2])
--- warcraftlogs.py
+++ warcraftlogs.py
@@ -5,60 +5,60 @@ ENDERECOLOG = 'https://www.warcraftlogs.com/reports/{reportid}'
 SITE = 'https://www.warcraftlogs.com:443/v1/reports/user/{usuario}?api_key=4615d3b11c614e608c6f294610f81fef'
 
 jsonstore = json_store_client.Client(
-    'https://www.jsonstore.io/63816dcc35a4ec5cb91d112b23a8c3dafcee418e2736fdf509df97aa8b552e37'
+	'https://www.jsonstore.io/63816dcc35a4ec5cb91d112b23a8c3dafcee418e2736fdf509df97aa8b552e37'
 )
 
 def converterData(dataepoch):
-    import time
-    return time.strftime("%d/%m/%Y %H:%M:%S", time.localtime(dataepoch / 1000))
+	import time
+	return time.strftime("%d/%m/%Y %H:%M:%S", time.localtime(dataepoch / 1000))
 
 def iniciarlizarDb(nome):
-    db = jsonstore.retrieve(nome)
-    if not db:
-        jsonstore.store(nome, {'ultimos': '0'})
-        db = jsonstore.retrieve(nome)
+	db = jsonstore.retrieve(nome)
+	if not db:
+		jsonstore.store(nome, {'ultimos': '0'})
+		db = jsonstore.retrieve(nome)
 
-    return db
+	return db
 
 
 def texto(dados):
-    texto = ':newspaper: Log:{log} \towner:{owner} \tData:{data}'.format(
-        log=ENDERECOLOG.format(reportid=dados['id']),
-        owner=dados['owner'],
-        data=converterData(dados['end']))
-    return texto
+	texto = ':newspaper: Log:{log} \towner:{owner} \tData:{data}'.format(
+		log=ENDERECOLOG.format(reportid=dados['id']),
+		owner=dados['owner'],
+		data=converterData(dados['end']))
+	return texto
 
 
 def atualizarContador():
-    db = jsonstore.retrieve('exec')
+	db = jsonstore.retrieve('exec')
 
-    contador = db['count']
-    print(str(contador))
-    jsonstore.store('exec', {'count': contador + 1})
+	contador = db['count']
+	print(str(contador))
+	jsonstore.store('exec', {'count': contador + 1})
 
 
 def buscarLogs(nome):
-    url = SITE.format(usuario=nome)
-    response = requests.get(url)
-    data = []
-    data = response.json()
+	url = SITE.format(usuario=nome)
+	response = requests.get(url)
+	data = []
+	data = response.json()
 
-    db = iniciarlizarDb(nome)
+	db = iniciarlizarDb(nome)
 
-    listadb = []
-    listalogs = []
+	listadb = []
+	listalogs = []
 
-    for itens in data:
-        listadb.append(itens['id'])
-        if itens['id'] not in db['ultimos']:
-            listalogs.append(texto(itens))
+	for itens in data:
+		listadb.append(itens['id'])
+		if itens['id'] not in db['ultimos']:
+			listalogs.append(texto(itens))
 
    # print(listadb)
 
-    jsonstore.store(nome, {'ultimos': listadb})
+	jsonstore.store(nome, {'ultimos': listadb})
 
-    if len(listalogs) >= 1:
-      atualizarContador()
-      print(listadb)
+	if len(listalogs) >= 1:
+	  atualizarContador()
+	  print(listadb)
 
-    return listalogs
+	return listalogs
