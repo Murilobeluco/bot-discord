85563fb5ca5b896a7326260b79a001cf3b62094d
Murilo <Murilobeluco@yahoo.com>
2020-03-15 01:29:23 -0300

    novo comando para gerar qrcode

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ import ffmpeg
 import os
 import time
 import psutil
+import urllib.parse
 
 BOT_PREFIX = ('!')
 
@@ -43,76 +44,83 @@ async def tocaraudio(ctx, audio=''):
 	else:
 		await ctx.send('Bot já conectado em uma sala.')
 	 
-@client.command(pass_context=True)
+@client.command()
 async def sheriff(ctx):
 	'retorna um sheriff feito de emoji.'
 	mensagem = '{0.author.mention}, este é seu Random Sheriff Emoji: \n {sheriff}'.format(
 		ctx.message, sheriff=desenho(random_emoji()))
 	await ctx.send(mensagem)
 
-@client.command(pass_context=True)
+@client.command()
+async def qrcode(ctx, arg1):
+	e = discord.Embed()
+	texto = urllib.parse.quote_plus(arg1)
+	e.set_image(url='http://api.qrserver.com/v1/create-qr-code/?color=9EFFC5&bgcolor=000000&data={param}&qzone=3&margin=0&size=450x450&ecc=M'.format(param=texto))
+	await ctx.send(embed=e)
+
+@client.command()
 async def pedra(ctx):
    'toca um audio randômico chamando o grupo para fazer pedra.'
    await tocaraudio(ctx, rng(['audios/caralhovamosfazerpedra.mp3', 'audios/cadepedra.mp3', 'audios/vamosfazerpedra.mp3']))
 
-@client.command(pass_context=True)
+@client.command()
 async def blizz(ctx):
 	'toca um audio randômico que fala verdades sobre a blizzard.'
 	await tocaraudio(ctx, rng(['audios/infoblizzard.mp3', 'audios/wowbug.mp3', 'audios/wowbugado.mp3']))
 
-@client.command(pass_context=True)
+@client.command()
 async def bfa(ctx):
 	'toca um audio pedindo o que todo mundo deseja de bfa: ..... o seu FIM!'
 	await tocaraudio(ctx, 'audios/fimbfa.mp3')
 
-@client.command(pass_context=True)
+@client.command()
 async def maedacarol(ctx):
 	'chama a mãe da carol.'
 	await tocaraudio(ctx, rng(['audios/obrigacao.mp3', 'audios/maedacarol.mp3']))
 
-@client.command(pass_context=True)
+@client.command()
 async def nerfa(ctx):
 	'toca um audio pedindo para nerfar o que é quebrado!'
 	await tocaraudio(ctx, 'audios/broken.mp3')
 
-@client.command(pass_context=True)
+@client.command()
 async def amanha(ctx):
 	'toca um audio com a famosa frase do Semente quando é perguntado a ele se ele quer fazer ilha'
 	await tocaraudio(ctx, 'audios/amanha.mp3')
 
-@client.command(pass_context=True)
+@client.command()
 async def livros(ctx):
 	'toca um audio com a fala da boss Bella de lower Karazhan!'
 	await tocaraudio(ctx, 'audios/livros.mp3')
 
-@client.command(pass_context=True)
+@client.command()
 async def valkyr(ctx):
 	'toca um audio em homenagem a um famoso druida que jogava cliando nas skill!'
 	await tocaraudio(ctx, 'audios/mare.mp3')
 
-@client.command(pass_context=True)
+@client.command()
 async def oak(ctx):
 	'toca um audio com a fala do boss Oakheart de Darkheart Thicket.'
 	await tocaraudio(ctx, 'audios/oak.mp3')
 
-@client.command(pass_context=True)
+@client.command()
 async def xixizinho(ctx):
 	'toca um audio com a fala do boss Rokmora da melhor dungeon de legion: Neltharion''s Lair'
 	await tocaraudio(ctx, 'audios/xixizinho.mp3')
 
-@client.command(pass_context=True)
+@client.command()
 async def tosco(ctx):
 	'toca um audio com a fala da boss Bella de lower Karazhan!'
 	await tocaraudio(ctx, 'audios/tosco.mp3')
 
-@client.command(pass_context=True)
+@client.command()
 async def dorime(ctx):
 	'toca dorime'
 	await tocaraudio(ctx, 'audios/dorime.mp3')
 	with open('img/dorime.jpg', 'rb') as fp:
 		await ctx.send(file=discord.File(fp, 'dorime.jpg'))
 
-@client.command(pass_context=True)
+@client.command()
 async def tururu(ctx):
 	'toca a musica triste do naruto'
 	await tocaraudio(ctx, 'audios/tururu.mp3')
@@ -120,19 +128,19 @@ async def tururu(ctx):
 		message = await ctx.send(file=discord.File(fp, 'naruto.png'))
 		await message.add_reaction('\N{LOUDLY CRYING FACE}')
 
-@client.command(pass_context=True)
+@client.command()
 async def gaucho(ctx):
 	'toca um audio de um gaucho falando verdades sobre armandinho'
 	await tocaraudio(ctx, 'audios/gaucho.mp3')
 
-@client.command(pass_context=True)
+@client.command()
 async def saizica(ctx):
 	'toca um audio da boss galindra de lower karazhan'
 	await tocaraudio(ctx, 'audios/zica.mp3')
 	with open('img/titus.gif', 'rb') as fp:
 		await ctx.send(file=discord.File(fp, 'titus.gif'))
 
-@client.command(pass_context=True, aliases=['essencias', 'essences', 'iongod'])
+@client.command(aliases=['essencias', 'essences', 'iongod'])
 async def milagre(ctx):
 	'posta uma imagem de um milagre'
 	await tocaraudio(ctx, 'audios/milagre.mp3')
@@ -140,29 +148,29 @@ async def milagre(ctx):
 		await ctx.send('A prova que milagres existem!')
 		await ctx.send(file=discord.File(fp, 'essence.png'))
 
-@client.command(pass_context=True)
+@client.command()
 async def choras(ctx, arg1):
 	arquivo = cria_audio(arg1[:20])
 	await tocaraudio(ctx, arquivo)
 	await deleta_arquivo(arquivo)
 
-@client.command(pass_context=True)
+@client.command()
 async def drogas(ctx):
 	'toca um audio avisando que chegou drogas'
 	await tocaraudio(ctx, 'audios/droga.mp3')
 
-@client.command(pass_context=True)
+@client.command()
 async def heart(ctx):
 	'toca um audio em homenagem ao Heart'
 	await tocaraudio(ctx, 'audios/heart.mp3')
 
-@client.command(pass_context=True)
+@client.command()
 async def turtle(ctx):
 	'toca a musica A Turtle Made It to the Water!'
 	await tocaraudio(ctx, 'audios/turtle.mp3')
 	await ctx.send(turtle_emoji())
 
-@client.command(pass_context=True)
+@client.command()
 async def ping(ctx):
 	'mostra o ping do bot com o servidor do discord'
 	before = time.monotonic()
